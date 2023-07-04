51e48c3fb9d154ab37d5f47c7d0524c8a6caa54d
Ojo Favour Oluwatoba <ojofavouroluwatoba@gmail.com>
2023-07-04 19:16:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const dayInput = document.getElementById("day");
+const monthInput = document.getElementById("month");
+const yearInput = document.getElementById("year");
+
+const answerButton = document.getElementById("result");
+const ageOutput = document.getElementById("end");
+
+//const errorMessage = document.getElementById("message");
+
+answerButton.addEventListener("click", calculateAge);
+
+const calculateAge = () => {
+  const day = parseInt(dayInput.value);
+  const month = parseInt(monthInput.value);
+  const year = parseInt(yearInput.value);
+
+  if (isNaN(day) || isNaN(month) || isNaN(year)) {
+    errorMessage.textContent =
+      "Please enter valid day, month, and year values.";
+    return;
+  }
+
+  const birthDate = new Date (year, month - 1, day);
+  const today = new Date ();
+  const age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate () < birthDate.getDate())) {
+    age --;
+  }
+  //console.log (today);
+
+}
