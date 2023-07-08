623fd5390347b16e71f6f47475aed5587134c7df
Ojo Favour Oluwatoba <ojofavouroluwatoba@gmail.com>
2023-07-08 11:21:32 +0100

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const monthInput = document.getElementById("month");
 const yearInput = document.getElementById("year");
 
 const answerButton = document.getElementById("result");
-const ageOutput = document.getElementById("end");
+const ageOutput = document.getElementB;
+
+yId("end");
 
 //const errorMessage = document.getElementById("message");
 
@@ -14,20 +16,22 @@ const calculateAge = () => {
   const month = parseInt(monthInput.value);
   const year = parseInt(yearInput.value);
 
-  if (isNaN(day) || isNaN(month) || isNaN(year)) {
-    errorMessage.textContent =
-      "Please enter valid day, month, and year values.";
+  if (isNaN(day)) {
+    displayError = "Please enter valid day, month, and year values.";
+    clearResults();
     return;
   }
 
-  const birthDate = new Date (year, month - 1, day);
-  const today = new Date ();
+  const birthDate = new Date(year, month - 1, day);
+  const today = new Date();
   const age = today.getFullYear() - birthDate.getFullYear();
   const monthDiff = today.getMonth() - birthDate.getMonth();
 
-  if (monthDiff < 0 || (monthDiff === 0 && today.getDate () < birthDate.getDate())) {
-    age --;
+  if (
+    monthDiff < 0 ||
+    (monthDiff === 0 && today.getDate() < birthDate.getDate())
+  ) {
+    age--;
   }
   //console.log (today);
-
-}
+};
